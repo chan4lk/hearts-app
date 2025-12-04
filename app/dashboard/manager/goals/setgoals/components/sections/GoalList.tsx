a769129738ae3ae45f7fb1b220bcc4efb88b1ec9
Thilan Buddhika <thilan@bistecglobal.com>
2025-12-05 00:21:29 +0530

    fix: Update showManager prop in GoalList component to false
    - Changed the showManager prop from true to false in the GoalList component, adjusting the visibility of manager-related features in the goals display.

--- app/dashboard/manager/goals/setgoals/components/sections/GoalList.tsx
+++ app/dashboard/manager/goals/setgoals/components/sections/GoalList.tsx
@@ -43,7 +43,7 @@ export function GoalList({
             goals={filteredGoals}
             onGoalClick={onViewGoal}
             showEmployee={true}
-            showManager={true}
+            showManager={false}
             disableStatusUpdate={true}
           />
         </div>
